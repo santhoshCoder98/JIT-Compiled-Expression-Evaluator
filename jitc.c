fc23b549a1f7afe8f0c1fd6850c0c3a1704fa75d
Santhosh Chandrasekaran <santhosh98720@gmail.com>
2023-11-24 13:04:37 -0800

    JITC Update

--- jitc.c
+++ jitc.c
@@ -16,14 +16,69 @@
 
 /**
  * Needs:
- *   fork()
- *   execv()
- *   waitpid()
- *   WIFEXITED()
- *   WEXITSTATUS()
- *   dlopen()
- *   dlclose()
- *   dlsym()
+ *   fork() - Creates a new Child Process.
+ *   execv() - Function that overwrites the current process with a different process.
+ *   waitpid() - Checks if the Child Process has executed and exited.
+ *   WIFEXITED() - Checks if the Child Process exited normally.
+ *   WEXITSTATUS() Use in Compiler
+ *   dlopen() In Open Func - Opens a new file.
+ *   dlclose() - Closes an opened file.
+ *   dlsym() - Lookup a file for a specific symbol or character.
  */
+
+
+int jitc_compile(const char *input, const char *output) {
+    char *input2 = (char *)input;
+    char *output2 = (char *)output;
+    int status;
+    pid_t pid = fork();
+    if(pid == -1) {
+        TRACE("Fork Error");
+        return -1;
+    }
+    if(!pid) {
+        char *argv[] = {"gcc", "-O3", "-fpic", "-shared", "-o", NULL, NULL, "-lm", NULL};
+        argv[5] = output2;
+        argv[6] = input2;
+        execv("/usr/bin/gcc", argv);
+        TRACE("EXECV Error");
+        exit(0);
+    }
+    else {
+        if(waitpid(pid, &status, 0) == -1) {
+            TRACE("Waitpid Error");
+            return -1;
+        }
+        if(!WIFEXITED(status)) {
+            TRACE("WIFEXITED FAILED");
+            return -1;
+        }
+    }
+    return 0;
+}
+
+struct jitc *jitc_open(const char *pathname) {
+    void *cFile = dlopen(pathname, RTLD_LAZY | RTLD_LOCAL);
+    if(cFile == NULL) {
+        TRACE("Open Error");
+        return cFile;
+    }
+    return cFile;
+}
+
+void jitc_close(struct jitc *jitc) {
+    if(dlclose(jitc) != 0) {
+        TRACE("Close Error");
+    }
+}
+
+long jitc_lookup(struct jitc *jitc, const char *symbol) {
+    char *address = dlsym(jitc, symbol);
+    if(address == NULL) {
+        TRACE("Lookup");
+        return (long)address;
+    }
+    return (long)address;
+}
 
 /* research the above Needed API and design accordingly */
